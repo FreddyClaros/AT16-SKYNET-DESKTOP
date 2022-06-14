caede6cf223b2d576140136e60f1c1fb88c6ccb9
AlbertLlanos <intillanos2017@gmail.com>
2022-06-14 12:51:16 -0400

    Integration to pyQT with metadata convert

--- src/view/Convert_Service/components/main_metadata.py
+++ src/view/Convert_Service/components/main_metadata.py
@@ -12,7 +12,7 @@
 #
 
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPlainTextEdit, QLabel, QLineEdit, QPushButton, \
-    QComboBox, QSpacerItem, QSizePolicy, QFileDialog
+    QComboBox, QSpacerItem, QSizePolicy, QFileDialog, QTableWidget, QHeaderView, QAbstractItemView, QTableWidgetItem, QToolBar, QAction
 from src.view.Convert_Service.components.title import Title
 from src.view.Convert_Service.components.buttons_top import ButtonsTop
 
@@ -27,53 +27,41 @@ class MainMetadata(QWidget):
         self.setLayout(self.layout)
 
     def get_layout_body(self):
-
         body = QHBoxLayout()
         body.addLayout(self.left_layout(), 10)
         body.addLayout(self.right_layout(), 75)
         return body
 
     def right_layout(self):
-        show_button = QPushButton("Show Document")
+        self.table = QTableWidget()
+        self.setting_table()
+        self.editor = QPlainTextEdit()
         right = QVBoxLayout()
-        right.addWidget(QPlainTextEdit(), 75)
-        right.addWidget(show_button)
+        right.addWidget(self.table, 50)
+        right.addWidget(self.editor, 25)
         return right
 
     def left_layout(self):
-        list_convert = QComboBox()
-        list_convert.addItem("Metadata")
-        list_convert.addItem("Video")
-        list_convert.addItem("Translator")
-        list_convert.addItem("OCR")
-        list_convert.addItem("Image")
-        list_convert.addItem("Audio")
-        list_convert.addItem("Wav to Txt")
-
-        list_format = QComboBox()
-        list_format.addItem("txt")
-
+        self.list_convert = QComboBox()
+        self.list_convert.addItem("Metadata")
+        self.list_format = QComboBox()
+        self.list_format.addItem("txt")
         self.file_path = QLineEdit()
         self.file_path.setReadOnly(True)
-
         browse_button = QPushButton("Browse")
         browse_button.clicked.connect(self.browse_file)
-
-        convert_button = QPushButton("Convert")
-
+        self.convert_button = QPushButton("Convert")
         text_area_message = QPlainTextEdit()
-
         vertical_spacer = QSpacerItem(10, 600, QSizePolicy.Expanding)
-
         menu = QVBoxLayout()
         menu.addWidget(QLabel("Convert:"))
-        menu.addWidget(list_convert)
+        menu.addWidget(self.list_convert)
         menu.addWidget(QLabel("File Path:"))
         menu.addWidget(self.file_path)
         menu.addWidget(browse_button)
         menu.addWidget(QLabel("Output Format:"))
-        menu.addWidget(list_format)
-        menu.addWidget(convert_button)
+        menu.addWidget(self.list_format)
+        menu.addWidget(self.convert_button)
         menu.addSpacerItem(vertical_spacer)
         menu.addWidget(QLabel("Message:"))
         menu.addWidget(text_area_message)
@@ -82,3 +70,43 @@ class MainMetadata(QWidget):
     def browse_file(self):
         file_name = QFileDialog.getOpenFileName(self, 'Open file', 'D:\\', '')
         self.file_path.setText(file_name[0])
+
+    def get_file_path(self):
+        return str(self.file_path.text())
+
+    def get_list_convert(self):
+        return str(self.list_convert.currentText())
+
+    def get_convert_button(self):
+        return self.convert_button
+
+    def get_output_format(self):
+        return str(self.list_format.currentText())
+
+    def get_editor_text(self):
+        return self.editor
+
+    def get_table(self):
+        return self.table
+
+    def file_open3(self, path):
+        print(path)
+        with open(path, "r") as f:
+            text = f.read()
+        self.editor.setPlainText(text)
+
+    def setting_table(self):
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(('Link', 'Output Format', 'Status', 'Path Saved', 'Path Download'))
+        self.table.setColumnHidden(3, True)
+        self.table.setColumnHidden(4, True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+    def add_values_table(self, link, output_format, status, path_saved, path_download):
+        self.table.insertRow(self.table.rowCount())
+        self.table.setItem(self.table.rowCount() - 1, 0, QTableWidgetItem(link))
+        self.table.setItem(self.table.rowCount() - 1, 1, QTableWidgetItem(output_format))
+        self.table.setItem(self.table.rowCount() - 1, 2, QTableWidgetItem(status))
+        self.table.setItem(self.table.rowCount() - 1, 3, QTableWidgetItem(path_saved))
+        self.table.setItem(self.table.rowCount() - 1, 4, QTableWidgetItem(path_download))
